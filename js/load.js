2d2e8211c3a7a85319b97b5d49c48db584b617cc
Sehdi Nassim <hm_sehdi@esi.dz>
2020-03-05 15:12:48 +0100

    Added the loading page

---
+++ js/load.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded',(event)=>{
+    let body = document.getElementsByTagName('body')[0]
+    body.style.overflowY='scroll'
+    body.firstElementChild.remove()
+})
