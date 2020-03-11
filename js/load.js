956e992b65b5e799e94cdbc368213f8f9af5f568
Sehdi Nassim <hm_sehdi@esi.dz>
2020-03-11 17:19:42 +0100

    Updated speakers, about and gallery section

--- js/load.js
+++ js/load.js
@@ -1,5 +1,6 @@
 window.onload=()=>{
     let body = document.getElementsByTagName('body')[0]
     body.style.overflowY='scroll'
-    body.firstElementChild.remove()
+    body.firstElementChild.style.display='none'
+    body.firstElementChild.style.animation='none'
 }
