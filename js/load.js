e5038d2d1207183f62cfedb0225d9bde3bd6483b
Sehdi Nassim <hm_sehdi@esi.dz>
2020-03-12 01:52:54 +0100

    Fixed the loader

--- js/load.js
+++ js/load.js
@@ -1,6 +1,5 @@
 window.onload=()=>{
     let body = document.getElementsByTagName('body')[0]
     body.style.overflowY='scroll'
-    body.firstElementChild.style.display='none'
-    body.firstElementChild.style.animation='none'
+    body.firstElementChild.firstElementChild.remove()
 }
