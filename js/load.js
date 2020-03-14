b9db30adc753fb3742f513a7a956d6e0208e830b
Mouad Benmoussat <hm_benmoussat@esi.dz>
2020-03-14 22:25:42 +0100

    Hero and footer are fully responsive. Fixed some bugs.

--- js/load.js
+++ js/load.js
@@ -2,7 +2,7 @@ window.onload=()=>{
     let body = document.getElementsByTagName('body')[0]
     body.style.overflowY='scroll'
     body.firstElementChild.firstElementChild.remove()
-    body.firstElementChild.remove()
+
     let hero = document.getElementById('hero');
     let heroBg = hero.getElementsByClassName('hero-bg')[0];
     heroBg.classList.add("animation-chake")
